5ee43e38b7a87b6a6fe9c3686920278a41f31892
jaredgorski <jared.gorski@liferay.cloud>
2020-08-26 22:34:12 -0700

    Update run_mnist.py

--- run_mnist.py
+++ run_mnist.py
@@ -1,38 +1,68 @@
 import mnist
 import numpy as np
+import pickle
 
 import cnn
 from cnn.layers.conv import ConvLayer
 from cnn.layers.maxpool import MaxPoolLayer
 
-training_images = mnist.train_images()[:1000]
-training_labels = mnist.train_labels()[:1000]
+training_images = mnist.train_images()[:5000]
+training_labels = mnist.train_labels()[:5000]
 
-layers = [
-    ConvLayer(num_kernels=8),
-    MaxPoolLayer(),
-]
+## uncomment below to train mnist images as RGB data
+# import cv2
+# training_images_rgb = []
+# for i, image in enumerate(training_images):
+#     training_images_rgb.append(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB))
+# training_images = np.array(training_images_rgb)
+
 classes = [x for x in range(10)]
 
 # initialize
-n = cnn.CNN(layers)
+net = None
+answer = input("Would you like to load a model? (enter 'y' to load): ")
+should_load = answer == 'y'
+if should_load:
+    filename = input("Enter a filename (without the extension): ")
+    pickle_in = open(f'{filename}.pickle','rb')
+    net = pickle.load(pickle_in)
+else:
+    layers = [
+        ConvLayer(num_kernels=16),
+        MaxPoolLayer(),
+    ]
+    net = cnn.CNN(layers)
 
 # train
-n.train(training_images, training_labels, classes, num_epochs=3, rate=0.005)
+answer = input("Would you like to train? (enter 'y' to train): ")
+should_train = answer == 'y'
+if should_train:
+    net.train(training_images, training_labels, classes, num_epochs=3, rate=0.005)
 
 # predict
-print('\n\n>>> Testing model...\n')
-test_images = mnist.test_images()[:500]
-test_labels = mnist.test_labels()[:500]
+answer = input("Would you like to test the model? (enter 'y' to test): ")
+should_test = answer == 'y'
+if should_test:
+    print('\n\n>>> Testing model...\n')
+    test_images = mnist.test_images()[:1000]
+    test_labels = mnist.test_labels()[:1000]
 
-num_correct = 0
-for image, label in zip(test_images, test_labels):
-  prediction_index = n.predict(image)
+    num_correct = 0
+    for image, label in zip(test_images, test_labels):
+        prediction_index = net.predict(image)
 
-  prediction = classes[prediction_index]
-  correct_add = 1 if prediction == label else 0
-  num_correct += correct_add
+        prediction = classes[prediction_index]
+        correct_add = 1 if prediction == label else 0
+        num_correct += correct_add
 
-num_tests = len(test_images)
-percent_accurate = round(((num_correct / num_tests) * 100), 3)
-print(f'Prediction accuracy ({num_tests} attempts): {percent_accurate}%\n')
+    num_tests = len(test_images)
+    percent_accurate = round(((num_correct / num_tests) * 100), 3)
+    print(f'Prediction accuracy ({num_tests} attempts): {percent_accurate}%\n')
+
+# save model
+answer = input("Would you like to save the model? (enter 'y' to save): ")
+should_save = answer == 'y'
+if should_save:
+    filename = input("Enter a filename (without the extension): ")
+    with open(f'{filename}.pickle','wb') as f:
+        pickle.dump(net, f)
